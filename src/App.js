af82dcaf162a2168bb5993289af92750ed7b9af1
unknown <slupiktomasz@gmail.com>
2022-09-09 07:53:17 +0200

    ice-cream-app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import Line from './components/line/Line';
+import './App.css';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Smakołkaki</h1>
+      <Line />
+
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/line/Line.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Line.css'
+
+
+
+function Line () {
+
+return (
+    <div>
+        <div className="linebox">
+            <div className="line"></div>
+        </div>
+    </div>   
+    )
+}
+
+export default Line
+
